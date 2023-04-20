30d44c2fcf9c5a695c3031e488b0d87b0a117592
Damian Kliber <damiankliber@gmail.com>
2023-04-20 14:48:28 +0200

    Add wait function

---
+++ src/utils/defines/GlobalFunctions.ts
@@ -0,0 +1,9 @@
+const wait = (milliseconds: number): Promise<void> => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve()
+  }, milliseconds)
+})
+
+export default {
+  wait,
+}
--- src/utils/defines/index.ts
+++ src/utils/defines/index.ts
@@ -1,8 +1,10 @@
 import Paths from './paths'
+import GlobalFunctions from './GlobalFunctions'
 
 export * from './Errors'
 
 export default {
   Paths,
   Error,
+  GlobalFunctions,
 }
