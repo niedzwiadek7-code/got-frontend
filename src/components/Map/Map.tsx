080d62168ba94d5b22883233e7a032934bcad217
Damian Kliber <damiankliber@gmail.com>
2023-05-08 10:18:29 +0200

    serve a point in the map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import {
   MapContainer, Marker, Popup, TileLayer,
 } from 'react-leaflet'
-import Elements from './Elements'
 import 'leaflet/dist/leaflet.css'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import './styles.scss'
+import { divIcon } from 'leaflet'
+import Styles from './Map.module.scss'
+import Elements from './Elements'
 
 type Props = {
   // @ts-ignore
@@ -11,13 +17,24 @@ type Props = {
 }
 
 const Map: React.FC<Props> = (props) => {
-  const center = props.points?.[0] ? props.points[0].getPosition() : [0, 0]
+  const center = props.points?.[0] ? props.points[0].getPosition() : [50, 20]
+
+  const iconMarkup = renderToStaticMarkup(
+    <FontAwesomeIcon
+      icon={faLocationDot}
+      className={Styles.marker}
+    />,
+  )
+
+  const customMarkerIcon = divIcon({
+    html: iconMarkup,
+  })
 
   return (
     <MapContainer
       center={center}
       zoom={13}
-      scrollWheelZoom={false}
+      scrollWheelZoom
       style={{ height: '100%' }}
     >
       <TileLayer
@@ -27,7 +44,8 @@ const Map: React.FC<Props> = (props) => {
       {
         (props.points || []).map((point) => (
           <Marker
-            position={point.position}
+            icon={customMarkerIcon}
+            position={point.getPosition()}
             key={point.name}
           >
             <Popup>
