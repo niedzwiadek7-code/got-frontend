b62c4bdf859b6932e90a6510bbcdca644f06fe05
Damian Kliber <damiankliber@gmail.com>
2023-05-08 10:25:46 +0200

    add map to create terrain point form

--- src/pages/ManageMap/Panels/TerrainPoint/Create/TerrainPoint.tsx
+++ src/pages/ManageMap/Panels/TerrainPoint/Create/TerrainPoint.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { toast } from 'react-toastify'
@@ -8,6 +8,7 @@ import * as Input from '../../../../../components/UI/Input'
 import TextArea from '../../../../../components/UI/TextArea'
 import { Errors, getPath, PathNames } from '../../../../../utils/defines'
 import TerrainPointService from '../../../../../services/TerrainPointService'
+import MapDefinition from '../../../../../components/Map'
 
 type Inputs = {
   name: string,
@@ -24,6 +25,15 @@ const TerrainPoint: React.FC<Props> = () => {
   const {
     register, handleSubmit, formState: { errors },
   } = useForm<Inputs>()
+  // @ts-ignore
+  const [mapPoint, setMapPoint] = useState<MapDefinition.Elements.Point>(
+    new MapDefinition.Elements.Point(
+      '',
+      '0',
+      '0',
+    ),
+  )
+
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     const terrainPointService = new TerrainPointService()
     const terrainPoint = await terrainPointService.createTerrainPoint(data)
@@ -51,62 +61,102 @@ const TerrainPoint: React.FC<Props> = () => {
   ])
 
   return (
-    <div className="w-50">
+    <div>
       <h2 className="mb-4"> Dodaj Punkt </h2>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3">
-          <Input.Component
-            label="Nazwa punktu"
-            type={Input.Type.TEXT}
-            data={register('name', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
-            errorMessage={errors?.name?.message || undefined}
-          />
-        </div>
-
-        <div className="mb-3">
-          <TextArea.Component
-            label="Opis"
-            height={150}
-            data={register('description', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
-            errorMessage={errors?.description?.message || undefined}
-          />
-        </div>
-
-        <div className="mb-3">
-          <Input.Component
-            label="Wysokość nad poziomem morza"
-            type={Input.Type.NUMBER}
-            data={register('sea_level_height', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
-            errorMessage={errors?.sea_level_height?.message || undefined}
-          />
-        </div>
-
-        <div className="mb-3">
-          <Input.Component
-            label="Szerokość geograficzna"
-            type={Input.Type.TEXT}
-            data={register('latitude', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
-            errorMessage={errors?.latitude?.message || undefined}
-          />
-        </div>
-
-        <div className="mb-3">
-          <Input.Component
-            label="Długość geograficzna"
-            type={Input.Type.TEXT}
-            data={register('longitude', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
-            errorMessage={errors?.longitude?.message || undefined}
-          />
-        </div>
-
-        <Button
-          type="submit"
-          variant="success"
+      <div className="row">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="col-6"
         >
-          Zapisz punkt
-        </Button>
-      </form>
+          <div className="mb-3">
+            <Input.Component
+              label="Nazwa punktu"
+              type={Input.Type.TEXT}
+              data={register('name', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
+              errorMessage={errors?.name?.message || undefined}
+              onChange={(e) => {
+                setMapPoint(
+                  new MapDefinition.Elements.Point(
+                    e.target.value,
+                    mapPoint.latitude,
+                    mapPoint.longitude,
+                  ),
+                )
+              }}
+            />
+          </div>
+
+          <div className="mb-3">
+            <TextArea.Component
+              label="Opis"
+              height={150}
+              data={register('description', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
+              errorMessage={errors?.description?.message || undefined}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input.Component
+              label="Wysokość nad poziomem morza"
+              type={Input.Type.NUMBER}
+              data={register('sea_level_height', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
+              errorMessage={errors?.sea_level_height?.message || undefined}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input.Component
+              label="Szerokość geograficzna"
+              type={Input.Type.TEXT}
+              data={register('latitude', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
+              errorMessage={errors?.latitude?.message || undefined}
+              onChange={(e) => {
+                setMapPoint(
+                  new MapDefinition.Elements.Point(
+                    mapPoint.name,
+                    e.target.value,
+                    mapPoint.longitude,
+                  ),
+                )
+              }}
+            />
+          </div>
+
+          <div className="mb-3">
+            <Input.Component
+              label="Długość geograficzna"
+              type={Input.Type.TEXT}
+              data={register('longitude', { required: ErrorMessageMap.get(Errors.REQUIRED) })}
+              errorMessage={errors?.longitude?.message || undefined}
+              onChange={(e) => {
+                setMapPoint(
+                  new MapDefinition.Elements.Point(
+                    mapPoint.name,
+                    mapPoint.latitude,
+                    e.target.value,
+                  ),
+                )
+              }}
+            />
+          </div>
+
+          <Button
+            type="submit"
+            variant="success"
+          >
+            Zapisz punkt
+          </Button>
+        </form>
+
+        <div
+          className="col-6"
+        >
+          <MapDefinition.Component
+            points={[mapPoint]}
+          />
+        </div>
+      </div>
     </div>
   )
 }
