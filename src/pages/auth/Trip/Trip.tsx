aab8f20572231b9697cc2ae97f9548227dc61c01
Damian Kliber <damiankliber@gmail.com>
2023-06-05 13:08:31 +0200

    create function in trip service to get all user trips

--- src/pages/auth/Trip/Trip.tsx
+++ src/pages/auth/Trip/Trip.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDependencies } from '../../../context/dependencies'
+import { useAuth } from '../../../context/auth'
+import Trip from '@/models/Trip'
+import * as Loading from '../../../components/UI/Loading'
 
 type Props = {}
 
-const Trip: React.FC<Props> = () => (
-  <div>
-    Sprzedam Opla
-  </div>
-)
+const TripComponent: React.FC<Props> = () => {
+  const { getApiService } = useDependencies()
+  const apiService = getApiService()
+  const { token } = useAuth()
+  const [trips, setTrips] = useState<Trip[]>()
+  const [loading, setLoading] = useState<boolean>(true)
 
-export default Trip
+  useEffect(() => {
+    const fetchData = async () => {
+      const tripService = apiService.getTrip(token)
+      setTrips(
+        await tripService.getAllTrips(),
+      )
+      setLoading(false)
+    }
+
+    fetchData()
+  })
+
+  if (loading) {
+    return <Loading.Component />
+  }
+
+  return (
+    <div>
+      {JSON.stringify(trips)}
+    </div>
+  )
+}
+
+export default TripComponent
