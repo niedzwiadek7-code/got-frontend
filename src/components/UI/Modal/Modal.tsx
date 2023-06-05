6d597c14d18f768d3a9b19e2ed7eafec201b6a3e
Damian Kliber <damiankliber@gmail.com>
2023-06-05 14:38:45 +0200

    create modal component

---
+++ src/components/UI/Modal/Modal.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { Modal, Button } from 'react-bootstrap'
+
+type Props = {
+  title: string,
+  message: string,
+  action: () => void,
+  variant: string,
+}
+
+const ModalComponent: React.FC<Props> = (props) => {
+  const [show, setShow] = useState<boolean>(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const action = async () => {
+    await props.action()
+    handleClose()
+  }
+
+  return (
+    <>
+      <Button
+        type="button"
+        variant={props.variant}
+        onClick={handleShow}
+      >
+        {props.title}
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {props.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {props.message}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cofnij
+          </Button>
+          <Button variant={props.variant} onClick={action}>
+            {props.title}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default ModalComponent
---
+++ src/components/UI/Modal/index.ts
@@ -0,0 +1,5 @@
+import Component from './Modal'
+
+export {
+  Component,
+}
