8244673acacf30948e72f1396519308d503ccd28
Damian Kliber <damiankliber@gmail.com>
2023-10-30 17:18:02 +0100

    improvement points list path

--- src/utils/defines/paths.ts
+++ src/utils/defines/paths.ts
@@ -43,7 +43,7 @@ export const PathLinkMap = new Map([
 
   [PathNames.MANAGE_MAP, '/manage-map'],
 
-  [PathNames.TERRAIN_POINT_LIST, '/terrain-points'],
+  [PathNames.TERRAIN_POINT_LIST, '/terrain-points/list'],
   [PathNames.TERRAIN_POINT_ADD, '/terrain-points/add'],
   [PathNames.TERRAIN_POINT_EDIT, '/terrain-points/edit/:id'],
   [PathNames.TERRAIN_POINT_DELETE, '/terrain-points/delete/:id'],
